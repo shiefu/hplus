d189bc81acb5734cbe38088055ab6f9e5cb8e085
Jeff <shiefu@gmail.com>
2019-09-12 15:18:43 +0800

    commit test

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -8,11 +8,9 @@
     <title><?=SITE_CNAME?> - 主页</title>
     <meta name="keywords" content="H+后台主题,后台bootstrap框架,会员中心主题,后台HTML,响应式后台">
     <meta name="description" content="H+是一个完全响应式，基于Bootstrap3最新版本开发的扁平化主题，她采用了主流的左右两栏式布局，使用了Html5+CSS3等现代技术">
-
     <!--[if lt IE 9]>
     <meta http-equiv="refresh" content="0;ie.html" />
     <![endif]-->
-
     <link rel="shortcut icon" href="<?=ADMIN_ASSETS_DIR?>/img/favicon.ico">
     <link href="<?=ADMIN_ASSETS_DIR?>/css/bootstrap.min14ed.css?v=3.3.6" rel="stylesheet">
     <link href="<?=ADMIN_ASSETS_DIR?>/css/font-awesome.min93e3.css?v=4.4.0" rel="stylesheet">
