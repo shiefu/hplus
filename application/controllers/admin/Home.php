58cdf2d33ca4a3d5523f35aeff056145c489e818
Jeff <shiefu@gmail.com>
2019-09-16 13:10:31 +0800

    修改登錄時更新last_login_time

--- application/controllers/admin/Home.php
+++ application/controllers/admin/Home.php
@@ -41,16 +41,34 @@ class Home extends CI_Controller {
             $message = '登录密码不能为空白';
             redirect(site_url(ADMIN_DIR.'/home/login/'.$message));
         }
-        // 檢查帳號、密碼跟資料庫是否相符
-        $query = $this->user_model->get_user(null, $account, $password, null);
+
+        $query = $this->user_model->get_user(null, $account, null, null);
+        // 登錄帳號存在
         if ($query->num_rows() > 0){
             $user = $query->row_array();
+            // 登錄密碼不正確
+            if (md5($password) != $user['password']){
+                $message = '登录密码不正确';
+                redirect(site_url(ADMIN_DIR.'/home/login/'.$message));
+            }
+            // 使用者狀態不是有效的
+            if ($user['status'] != 1){
+                $message = '帐号状态: '.$user['status_name'];
+                redirect(site_url(ADMIN_DIR.'/home/login/'.$message));
+            }
             unset($user['password']);
             $this->session->set_userdata($user);
+            $result = $this->user_model->update_user($user['id'], array('last_login_time'=>date('Y-m-d H:i:s')));
+            // 更新最後登錄時間失敗
+            if ($result['code'] != 0){
+                $message = $result['message'];
+                redirect(site_url(ADMIN_DIR.'/home/login/'.$message));
+            }
             redirect(site_url(ADMIN_DIR));
         }
+        // 登錄帳號不存在
         else {
-            $message = '登录帐号或密码不正确';
+            $message = '登录帐号不存在';
             redirect(site_url(ADMIN_DIR.'/home/login/'.$message));
         }
     }
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -29,4 +29,20 @@ class User_model extends CI_Model
 
         return $this->db->get();
     }
+
+    //==================================================================================================================
+    // 更新後台使用者資料
+    //==================================================================================================================
+    public function update_user($id, $data)
+    {
+        try {
+            $this->db->update('user', $data, array('id'=>$id));
+            $result = array('code'=>0, 'message'=>'更新使用者资料成功');
+        } catch (Exception $e) {
+            $db_error = $this->db->error();
+            $result = array('code'=>$db_error['code'], 'message'=>'更新使用者资料失败，'.$db_error['message']);
+        }
+
+        return $result;
+    }
 }
